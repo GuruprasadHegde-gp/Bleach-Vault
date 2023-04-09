a3d6f488c0598651f1ffa0fe495a5c0c58b641d6
Gautam-Hegde <gautamatjob@protonmail.com>
2023-04-09 20:03:54 +0530

    config mod+footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // 
 import React from "react";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 function App() {
 	return (
-		<div className="bg-black text-white">
+    <>
+		<div className=" font-Poppins bg-heroColor text-white">
       <NavBar/>
 			<div className="flex items-center justify-center min-h-screen">
-				<h1 className="text-4xl font-extrabold">Your Gateway to Web3 Commerce!"</h1>
+				<h1 className="font-Roboto text-4xl font-extrabold">Your Gateway to Web3 Commerce!"</h1>
 			</div>
 		</div>
+    <Footer/>
+    </>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      heroColor:'#f9f4f0'
+    },
+    extend: {
+      fontFamily: {
+          Roboto:["Roboto","sans-serif"],
+          Poppins:["Popping","sans-serif"],
+          Gothic:["Didact Gothic","sans-serif"],
+       },
+    },
   },
   plugins: [],
 }
