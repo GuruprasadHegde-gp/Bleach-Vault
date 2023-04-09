4bb2c3ffe123bd7abf513cfeb073178e455da161
Gautam-Hegde <gautamatjob@protonmail.com>
2023-04-09 18:54:50 +0530

    tailwind + rb wallet intgrn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="bg-black text-white">
+			<div className="flex items-center justify-between flex-row px-4 py-2">
+				{/* Logo */}
+				<h1 className="text-2xl font-bold">BLEACH VAULT</h1>
+				<ConnectButton />
+			</div>
+			<div className="flex items-center justify-center min-h-screen">
+				<h1 className="text-4xl font-extrabold">Your Gateway to Web3 Commerce!"</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;
