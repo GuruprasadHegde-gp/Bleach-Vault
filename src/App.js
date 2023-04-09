f15a20e278a9002b0191f629db88ce38fe56582c
Gautam-Hegde <gautamatjob@protonmail.com>
2023-04-09 19:32:34 +0530

    navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+// 
 import React from "react";
+import NavBar from "./components/NavBar";
 function App() {
 	return (
 		<div className="bg-black text-white">
-			<div className="flex items-center justify-between flex-row px-4 py-2">
-				{/* Logo */}
-				<h1 className="text-2xl font-bold">BLEACH VAULT</h1>
-				<ConnectButton />
-			</div>
+      <NavBar/>
 			<div className="flex items-center justify-center min-h-screen">
 				<h1 className="text-4xl font-extrabold">Your Gateway to Web3 Commerce!"</h1>
 			</div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="flex items-center justify-between flex-row px-4 py-2">
+    {/* Logo */}
+    <h1 className="text-2xl font-bold">3LEACH VAULT</h1>
+    <ConnectButton/>
+</div>
+  )
+}
+
+export default NavBar
